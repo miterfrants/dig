49bed7aed3cd8c0e6f42fce4eef48407954d8fcc
Po-Hsiang Huang <miterfrants@gmail.com>
2014-11-11 17:56:37 +0800

    first commit

---
+++ dig/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dig
+//
+//  Created by Po-Hsiang Huang on 2014/11/11.
+//  Copyright (c) 2014年 Po-Hsiang Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
